0dd56e55a61f6d5d2593a89f3c6d220d0a9f5d8b
krish choudhary <krish.choudhary_cs.aiml24@gla.ac.in>
2026-02-20 15:44:40 +0530

    Initialize pins and variables for ultrasonic sensor
    Defined TRIG and ECHO pins, and created variables for duration and distance.

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,18 @@
  * Measures distance using HC-SR04 ultrasonic sensor
  * and displays structured output via Serial Monitor.
  */
+// TODO 1:
+// Define TRIG pin (Use pin 9)
+const int TRIG_PIN = 9;
+
+// TODO 2:
+// Define ECHO pin (Use pin 10)
+const int ECHO_PIN = 10;
+
+// TODO 3:
+// Create variable to store duration
+long duration = 0;
+
+// TODO 4:
+// Create variable to store calculated distance
+float distance = 0.0;
